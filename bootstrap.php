1a8999b74443e66d54eee14104d38035cea85db3
papilooz <manriquequervie@gmail.com>
2025-07-11 16:30:56 +0800

    refactor(bootstrap.php): defining custom constants

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('UTILS_PATH', BASE_PATH . '/utils/');
+define('VENDOR_PATH', BASE_PATH . '/vendor/'); // <-- corrected
+define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
 
 chdir(BASE_PATH);
