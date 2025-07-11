2d474bd5daa0f8ffa1f5b48b62ab57e5a6e6a281
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-11 17:17:20 +0800

    feat: add env setter for setting environment variables

---
+++ utils/envSetter.util.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once BASE_PATH . '/bootstrap.php';
+require_once BASE_PATH . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
+$typeConfig = [
+    // PostgreSQL
+    'pgHost'     => $_ENV['PG_HOST'],
+    'pgPort'     => $_ENV['PG_PORT'],
+    'pgDb'       => $_ENV['PG_DB'],
+    'pgUser'     => $_ENV['PG_USER'],
+    'pgPassword' => $_ENV['PG_PASS'],
+
+    // MongoDB
+    'mongoUri'   => $_ENV['MONGO_URI'],
+    'mongoDB'    => $_ENV['MONGO_DB'],
+];
