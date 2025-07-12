6410a0f024dfde9a6ce2179de096d8c20ad21e30
papilooz <manriquequervie@gmail.com>
2025-07-12 21:54:05 +0800

    refactor(bootstrap.php): update constants

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,9 @@
 <?php
-define('BASE_PATH', realpath(__DIR__));
-define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
-define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
-define('DUMMIES_PATH', realpath(BASE_PATH . '/staticDatas/dummies'));
-define('DATABASE_PATH', realpath(BASE_PATH . '/database'));
-define('VENDOR_PATH', realpath(BASE_PATH . '/vendor'));
+define('BASE_PATH', dirname(__FILE__)); // more reliable
+define('HANDLERS_PATH', BASE_PATH . '/handlers');
+define('UTILS_PATH', BASE_PATH . '/utils');
+define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies');
+define('DATABASE_PATH', BASE_PATH . '/database');
+define('VENDOR_PATH', BASE_PATH . '/vendor');
+
 chdir(BASE_PATH);
