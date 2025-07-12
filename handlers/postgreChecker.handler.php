913f1b8d780129ede7bc6c74eb0e06e32aae471f
papilooz <manriquequervie@gmail.com>
2025-07-12 21:55:58 +0800

    refactor(postgre): improve and fix PostgreSQL checker handler

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,21 +1,22 @@
 <?php
 
-require_once UTILS_PATH . '/envSetter.util.php';
+require_once BASE_PATH . '/bootstrap.php'; // Make sure path constants are loaded
+$typeConfig = require_once UTILS_PATH . '/envSetter.util.php';
 
-$host = $typeConfig['pgHost'];
-$port = $typeConfig['pgPort'];
-$username = $typeConfig['pgUser'];
-$password = $typeConfig['pgPassword'];
-$dbname = $typeConfig['pgDb'];
+$host     = $typeConfig['pg_host'];
+$port     = $typeConfig['pg_port'];
+$username = $typeConfig['pg_user'];
+$password = $typeConfig['pg_pass'];
+$dbname   = $typeConfig['pg_db'];
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
 $dbconn = pg_connect($conn_string);
 
 if (!$dbconn) {
-    echo "❌ Connection Failed: ", pg_last_error() . "  <br>";
+    echo "❌ Connection Failed: " . pg_last_error() . "<br>";
     exit();
 } else {
-    echo "✔️ PostgreSQL Connection  <br>";
+    echo "✔️ PostgreSQL Connection<br>";
     pg_close($dbconn);
 }
