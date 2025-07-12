6447e9cf07b64bbea55dae75b2733fb3f707d67c
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-12 08:55:48 +0800

    feat: create dummy data for products

---
+++ staticDatas/products.staticData.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    [
+        'name'           => 'Baseball bat',
+        'description'    => 'Sturdy blunt weapon effective for close combat and self-defense.',
+        'category'       => 'Weapons',
+        'price'          => 10,
+        'stock_quantity' => 100,
+    ],
+    [
+        'name'           => 'Bandaid',
+        'description'    => 'Adhesive bandage used for covering minor cuts and wounds.',
+        'category'       => 'Medical',
+        'price'          => 10,
+        'stock_quantity' => 100,
+    ],
+        [
+        'name'           => 'Compass',
+        'description'    => 'Navigation tool used to determine direction and assist in orienteering.',
+        'category'       => 'Tools',
+        'price'          => 10,
+        'stock_quantity' => 100,
+    ],
+    [
+        'name'           => 'Chocolate',
+        'description'    => 'High-energy food item that boosts morale and provides quick calories.',
+        'category'       => 'Sustenance',
+        'price'          => 5,
+        'stock_quantity' => 100,
+    ],
+    [
+        'name'           => 'Beginner \'s Bundle',
+        'description'    => '3 weapons, 2 medical, 2 sustenance (one food, one water)',
+        'category'       => 'Sustenance',
+        'price'          => 150,
+        'stock_quantity' => 100,
+    ],
+];
