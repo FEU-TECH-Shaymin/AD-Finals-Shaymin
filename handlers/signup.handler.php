6f146944e87bd73fe35f35bb69751f72b0f3de1e
papilooz <manriquequervie@gmail.com>
2025-07-12 13:45:17 +0800

    refactor(signup.handler.php): update database utility path for consistency

--- handlers/signup.handler.php
+++ handlers/signup.handler.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../utils/db.php';
+require_once UTILS_PATH . '/database.util.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);   // Method Not Allowed
