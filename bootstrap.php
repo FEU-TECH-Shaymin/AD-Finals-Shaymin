724c40b8fe965526c11e61e05b5ff71772419f05
papilooz <manriquequervie@gmail.com>
2025-07-12 20:51:21 +0800

    refactor(bootstrap.php): adding vendors path

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
 define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
 define('DUMMIES_PATH', realpath(BASE_PATH . '/staticDatas/dummies'));
 define('DATABASE_PATH', realpath(BASE_PATH . '/database'));
-
+define('VENDOR_PATH', realpath(BASE_PATH . '/vendor'));
 chdir(BASE_PATH);
