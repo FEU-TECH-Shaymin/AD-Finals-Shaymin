62d31e4a7179ae86d0e366f5c1a5873089f50872
sdoxina <202311026@fit.edu.ph>
2025-07-13 09:20:55 +0800

    feat(footer.component.php): add branding and copyright

--- components/templates/footer.component.php
+++ components/templates/footer.component.php
@@ -21,6 +21,17 @@ function footerComponent(): void
                 <?php endforeach; ?>
               </ul>
             </div>
+            <div class="col-6 col-md-2 mb-3">
+              <h5>Web Designer</h5>
+              <h6>Shaymin</h6>
+            </div>
+            <div class="col-md-7 offset-md-1 mb-3">
+              <img src="/assets/img/outlastLogo.png" alt="Outlast Logo">
+            </div>
+          </div>
+          <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>Copyright © 2025 Outlast. All rights reserved.</p>
+          </div>
         </footer>
       </div>
     </footer>
