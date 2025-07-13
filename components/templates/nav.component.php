6abc30c32220991a554df895788b19f3ea25e483
sdoxina <202311026@fit.edu.ph>
2025-07-13 08:25:53 +0800

    feat(nav.component.php): include brand logo and link to home page

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -5,4 +5,7 @@ function navHeader(array $navList, ?array $user = null): void
 {
         ?>
     <nav class="navbar navbar-expand-lg fixed-top">
-      <div class="container-fluid">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto d-flex align-items-center" href="/index.php">
+                <img src="/assets/img/outlastLogo.png" alt="Outlast Logo" class="navbar-img">
+            </a>
