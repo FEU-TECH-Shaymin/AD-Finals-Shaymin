a3c61f3f11472f233967deb63ce21f0b9b8a37c6
sdoxina <202311026@fit.edu.ph>
2025-07-13 15:08:13 +0800

    feat(login-signup): add initial PHP view file

---
+++ pages/login-signup/index.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+// call the layout you want to use from layout folder
+require_once LAYOUTS_PATH . "/main.layout.php";
+// $mongoCheckerResult = require_once HANDLERS_PATH . "/mongodbChecker.handler.php";
+// $postgresqlCheckerResult = require_once HANDLERS_PATH . "/postgreChecker.handler.php";
+
+// Call layout renderer
+renderMainLayout(
+    function () {
+        ?>
+        <!-- Hero Section -->
+        <section class="section">
+            <div class="container-fluid">
+            </div>
+        </section>
+        <?php
+    },
+    [
+        "css" => [
+            "./assets/css/style.css"
+        ],
+        "js" => [
+            "./assets/js/script.js"
+        ]
+    ]
+);
+?>
