25f3e3a0aad253ad1f4f736fa150c3472a01c80a
sdoxina <202311026@fit.edu.ph>
2025-07-13 09:19:43 +0800

    feat(footer.component.php): add footer links dynamically from static data

--- components/templates/footer.component.php
+++ components/templates/footer.component.php
@@ -6,7 +6,24 @@ function footerComponent(): void
     // Load static footer data
     include_once STATICDATAS_PATH . '/footer.staticData.php';
     ?>
-    
+    <footer class="footer">
+      <div class="container">
+        <footer class="py-5">
+          <div class="row">
+            <div class="col-6 col-md-2 mb-3">
+              <ul class="footer flex-column list-unstyled">
+                <?php foreach ($footerLinks as $link): ?>
+                  <li class="footer-item mb-2">
+                    <a href="<?= htmlspecialchars($link['url']) ?>" class="footer-link p-0 text-body-secondary">
+                      <?= htmlspecialchars($link['label']) ?>
+                    </a>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+        </footer>
+      </div>
+    </footer>
     <?php
 }
 ?>
