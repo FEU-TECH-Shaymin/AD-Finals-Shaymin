9647e9936e1708cf1ee3a78b778961ed3b9775ed
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-13 11:01:23 +0800

    feat(bootstrap): add STATICDATAS_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,6 @@ define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
 define('DUMMIES_PATH', realpath(BASE_PATH . '/staticDatas/dummies'));
 define('DATABASE_PATH', realpath(BASE_PATH . '/database'));
 define('TEMPLATES_PATH', realpath(BASE_PATH . '/components/templates'));
-
+define('STATICDATAS_PATH', realpath(BASE_PATH . '/staticDatas'));
 
 chdir(BASE_PATH);
