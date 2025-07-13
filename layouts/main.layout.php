2ec8c3500043e7d06a69427aedc287491c0bb46d
sdoxina <202311026@fit.edu.ph>
2025-07-13 10:50:11 +0800

    refactor(main.layout.php): change unuse paths into comments

--- layouts/main.layout.php
+++ layouts/main.layout.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 // 1. Bootstrap, Autoload, Auth
 require_once BASE_PATH . '/bootstrap.php';
 require_once BASE_PATH . '/vendor/autoload.php';
-require_once UTILS_PATH . '/auth.util.php';
-Auth::init();
+// require_once UTILS_PATH . '/auth.util.php';
+// Auth::init();
 
 // 2. Load templates
 require_once TEMPLATES_PATH . '/head.component.php';
@@ -17,7 +17,7 @@ require_once UTILS_PATH . "/envSetter.util.php";
 require_once STATICDATAS_PATH . '/navPages.staticData.php';
 
 // 4. Determine current user
-$user = Auth::user();
+// $user = Auth::user();
 
 function renderMainLayout(callable $content, string $title, array $customJsCss = []): void
 {
