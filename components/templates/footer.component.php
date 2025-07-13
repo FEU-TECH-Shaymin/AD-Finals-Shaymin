fed9aba0024371f1ac05ddcad3f2ef49f0c335d4
sdoxina <202311026@fit.edu.ph>
2025-07-13 09:17:40 +0800

    feat(footer.component.php): include static footer data

--- components/templates/footer.component.php
+++ components/templates/footer.component.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 function footerComponent(): void
 {
+    // Load static footer data
+    include_once STATICDATAS_PATH . '/footer.staticData.php';
+    ?>
     
     <?php
 }
