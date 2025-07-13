219ce2796406932099dcb6964a1c221593098b76
sdoxina <202311026@fit.edu.ph>
2025-07-13 08:27:01 +0800

    feat(nav.component.php): implement offcanvas header for responsive mobile nav

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -9,3 +9,10 @@ function navHeader(array $navList, ?array $user = null): void
             <a class="navbar-brand me-auto d-flex align-items-center" href="/index.php">
                 <img src="/assets/img/outlastLogo.png" alt="Outlast Logo" class="navbar-img">
             </a>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title d-flex align-items-center" id="offcanvasNavbarLabel">
+                        <img src="/assets/img/outlastLgRed.png" alt="Outlast Logo">
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
