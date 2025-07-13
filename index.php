af0e7c5a7849038a1c499305c97d515fd04b555c
sdoxina <202311026@fit.edu.ph>
2025-07-13 09:51:56 +0800

    feat(landing): add mongo and postgre checkers

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 declare(strict_types=1);
 
-// Load layout system
+// call the layout you want to use from layout folder
 require_once LAYOUTS_PATH . "/main.layout.php";
+$mongoCheckerResult = require_once HANDLERS_PATH . "/mongodbChecker.handler.php";
+$postgresqlCheckerResult = require_once HANDLERS_PATH . "/postgreChecker.handler.php";
 
 
 ?>
