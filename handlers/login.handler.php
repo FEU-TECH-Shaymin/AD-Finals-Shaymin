cb5d8e9df52cc50f5deb224da6febd32b32ca678
papilooz <manriquequervie@gmail.com>
2025-07-13 16:00:29 +0800

    refactor(login.handler.php): adding username and password

--- handlers/login.handler.php
+++ handlers/login.handler.php
@@ -6,3 +6,5 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
     exit('Method Not Allowed');
 }
+$username = trim($_POST['username'] ?? '');
+$password = trim($_POST['password'] ?? '');
