155a2acd4e1f68be3e13f6a368218b6682f9b7fa
sdoxina <202311026@fit.edu.ph>
2025-07-13 08:44:46 +0800

    feat(head.component.php): add base HTML and page metadata with static title

---
+++ components/templates/head.component.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+function head(array $pageCss = []): void
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Outlast</title>
