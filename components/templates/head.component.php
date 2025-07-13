a1a6b05fafcdf848395ccb8efc450d4880320ff8
sdoxina <202311026@fit.edu.ph>
2025-07-13 08:46:30 +0800

    feat(head.component.php): load main and optional custom stylesheets

--- components/templates/head.component.php
+++ components/templates/head.component.php
@@ -16,3 +16,16 @@ function head(array $pageCss = []): void
         <!-- External CSS Libraries -->
         <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <!-- Main site CSS -->
+        <link rel="stylesheet" href="/assets/css/style.css">
+
+        <!-- Custom page-level CSS -->
+        <?php foreach ($pageCss as $cssFile): ?>
+            <link rel="stylesheet" href="<?= htmlspecialchars($cssFile) ?>">
+        <?php endforeach; ?>
+    </head>
+    <body>
+    <?php
+}
+?>
