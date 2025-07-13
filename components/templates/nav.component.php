ea912b7b096ac548e8ad20b315ac641565a1f158
sdoxina <202311026@fit.edu.ph>
2025-07-13 08:22:24 +0800

    feat(nav.component.php): add Bootstrap navbar structure with container

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -3,5 +3,6 @@ declare(strict_types=1);
 
 function navHeader(array $navList, ?array $user = null): void
 {
-
-?>
+        ?>
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container-fluid">
