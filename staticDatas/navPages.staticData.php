02f43f76de053c3b9308e652269229613a90ab18
sdoxina <202311026@fit.edu.ph>
2025-07-13 16:51:19 +0800

    fix(navPages.staticData.php): correct url for login page

--- staticDatas/navPages.staticData.php
+++ staticDatas/navPages.staticData.php
@@ -4,5 +4,5 @@ return [
     ["label" => "About", "url" => "/pages/about/index.php", "for" => "all"],
     ["label" => "Products", "url" => "/pages/products/index.php", "for" => "all"],
     ["label" => "Contact", "url" => "/pages/contact/index.php", "for" => "all"],
-    ["label" => "Login", "url" => "/pages/login-signup/index.php", "for" => "all"]
+    ["label" => "Login", "url" => "/pages/login/index.php", "for" => "all"]
 ];
