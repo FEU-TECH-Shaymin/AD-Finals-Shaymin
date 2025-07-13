0a9a3f5a4caf987f929bfa46324a2b57221492f8
sdoxina <202311026@fit.edu.ph>
2025-07-13 09:54:08 +0800

    feat(landing): add hero section

--- index.php
+++ index.php
@@ -10,7 +10,21 @@ $postgresqlCheckerResult = require_once HANDLERS_PATH . "/postgreChecker.handler
 renderMainLayout(
     function () {
         ?>
-        
+        <!-- Hero Section -->
+        <section class="hero-section">
+            <div class="container-fluid">
+                <div class="col hero-txt">
+                    <img src="./assets/img/outlastLogoWhite.png" alt="Outlast Logo" class="hero-img">
+                    <h1 class="hero-subheading">Survive the Horde. <br> Thrive in the Apocalypse.</h1>
+                    <p class="hero-body">
+                        When the world fell, we didn’t hide—we prepared. Get the ultimate survival gear. Trade in Zombie Crystals and secure your life.
+                    </p>
+                    <a href="./pages/products/index.php" class="btn btn-lg hero-cta">Our Products<i class="lni lni-arrow-right"></i></a>
+                </div>
+            </div>
+            <div id="image"></div>
+            <div id="glitch"></div>
+        </section>
         <?php
     },
     [
