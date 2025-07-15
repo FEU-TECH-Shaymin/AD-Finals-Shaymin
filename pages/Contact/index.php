a1b72203628fd731d23e23969fab5ccfd876fea4
gaganalexa <202310537@fit.edu.ph>
2025-07-15 22:14:39 +0800

    feat(contact): add contact hero section contents

--- pages/Contact/index.php
+++ pages/Contact/index.php
@@ -8,10 +8,17 @@ renderMainLayout(
         ?>
         <!--Contact Hero Section-->
         <section class="contact-hero-section">
-            
+            <div class="container text-center">
+                <h1>Need Help Surviving?</h1>
+                <p>
+                    Whether you're looking for gear recommendations or having trouble trading your Zombie Crystals, our squad is ready in the shadows—Outlast is here.
+                </p>
+                <a href="#contact-form" class="btn contact-cta">Contact Us</a>
+            </div>
         </section>
-    }
-)
 
-<?php
+        
+        <?php
+    },
+);
 ?>
