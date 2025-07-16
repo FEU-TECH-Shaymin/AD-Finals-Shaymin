3e595e7a685a3e3ef9fb731d824c2dbaee677dce
gaganalexa <202310537@fit.edu.ph>
2025-07-16 20:21:51 +0800

    feat(ExamplePage): add initial php page structure

---
+++ pages/ExamplePage/index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+// call the layout you want to use from layout folder
+require_once LAYOUTS_PATH . "/main.layout.php";
+// $mongoCheckerResult = require_once HANDLERS_PATH . "/mongodbChecker.handler.php";
+// $postgresqlCheckerResult = require_once HANDLERS_PATH . "/postgreChecker.handler.php";
+
+// Call layout renderer
+renderMainLayout(
+    function () {
+        ?>
+        <!-- PASTE YOUR CODE HERE -->
+        <?php
+    },
+    [
+        "css" => [
+            "./assets/css/style.css"
+        ],
+        "js" => [
+            "./assets/js/script.js"
+        ]
+    ]
+);
+?>
