7cf232262a52cf4705fcc4e60afd2de909ef12bd
sdoxina <202311026@fit.edu.ph>
2025-07-16 23:54:39 +0800

    fix(navPages.staticData.php): correct label from 'Signup' to 'Sign Up'

--- staticDatas/navPages.staticData.php
+++ staticDatas/navPages.staticData.php
@@ -8,7 +8,7 @@ return [
 
     // Guest Only
     ['label' => 'Login', 'url' => '/pages/login/index.php', 'guestOnly' => true],
-    ['label' => 'Signup', 'url' => '/pages/signup/index.php', 'guestOnly' => true],
+    ['label' => 'Sign Up', 'url' => '/pages/signup/index.php', 'guestOnly' => true],
 
     // Admin Only
     ['label' => 'Product Management', 'url' => '/pages/admin-products/index.php', 'authOnly' => true, 'role' => 'admin'],
