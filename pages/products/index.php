c37b54bd577b17acfa586424b487fa606fba09b9
nriveraaa <202310167@fit.edu.ph>
2025-07-17 08:15:37 +0800

    feat(products): add top bar

--- pages/products/index.php
+++ pages/products/index.php
@@ -16,7 +16,16 @@ renderMainLayout(
     function () use ($keyword, $products) {
         ?>
         <section class="all-categories-bg">
-            
+            <div class="top-bar">
+                <img src="images/outlastLogo.png" class="logo" alt="Outlast Logo">
+                <div class="search-section">
+                    <input type="text" class="search" placeholder="Search">
+                    <img src="images/search.png" class="search-icon" alt="Search">
+                </div>
+                <div class="icons">
+                    <img src="images/carttt.png" alt="Cart" class="icon-img">
+                </div>
+            </div>
         </section>
         <?php
     },
